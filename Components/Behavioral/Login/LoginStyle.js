86c00f81f45c66b95ecc6e11be43e045892c49c1
GarethSmall <gareth@chartlytics.com>
2018-02-18 18:00:42 -0600

    added flex property to fix height for login component

--- Components/Behavioral/Login/LoginStyle.js
+++ Components/Behavioral/Login/LoginStyle.js
@@ -2,13 +2,14 @@ import { StyleSheet } from 'react-native';
 import AppTheme from '../../../AppTheme/SendMe2017';
 
 export default LoginStyle = StyleSheet.create({
-  view : {
+  view: {
     backgroundColor: AppTheme.primaryColor,
+    flex: 1,
   },
   username : {
     height: 40,
     alignSelf: 'stretch',
     borderColor: 'gray',
-    borderWidth: 1
+    borderWidth: 1,
   },
 });
