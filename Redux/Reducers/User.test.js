5f321165ee91de01118875c48741a47aeac43f0b
GarethSmall <gareth@chartlytics.com>
2018-02-21 19:22:39 -0600

    Removed redundant variables

--- Redux/Reducers/User.test.js
+++ Redux/Reducers/User.test.js
@@ -11,18 +11,24 @@ describe('User Reducer', () => {
   });
 
   test('should not have affected stated from non existent type', () => {
-    Reducer(user).expect({type: 'THIS_TYPE_DOES_NOT_EXIST'}).toReturnState(initialState);
+    Reducer(user)
+      .expect({ type: 'THIS_TYPE_DOES_NOT_EXIST' })
+      .toReturnState(initialState);
   });
 
   test('should log user in', () => {
-    const existingState = initialState;
     const action = { type: ActionType.USER.LOGIN_SUCCESS };
-    Reducer(user).withState(existingState).expect(action).toReturnState({...initialState, isLoggedIn: true});
+    Reducer(user)
+      .withState(initialState)
+      .expect(action)
+      .toReturnState({ ...initialState, isLoggedIn: true });
   });
 
   test('should throw error message on bad login', () => {
-    const existingState = initialState;
-    const action = { type: ActionType.USER.LOGIN_ERROR, response: { message: 'Inccorect Username/Password'} };
-    Reducer(user).withState(existingState).expect(action).toReturnState({...initialState, loginError: 'Inccorect Username/Password'});
+    const action = { type: ActionType.USER.LOGIN_ERROR, response: { message: 'Inccorect Username/Password' } };
+    Reducer(user)
+      .withState(initialState)
+      .expect(action)
+      .toReturnState({ ...initialState, loginError: 'Inccorect Username/Password' });
   });
 });
